f4b93b13617693d96e373d55442bb24659756aeb
jhlee0222 <jhlee22@g.skku.edu>
2023-03-09 11:15:22 +0000

    add README, support datetime

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def twoSum(Self,nums,target):
+def twoSum(self,nums,target):
     for i in range(0,len(nums)):
         for j in range(i+1,len(nums)):
-            if nums[i]+nums[j]==target:
+            if nums[i] + nums[j] == target:
                 return [i,j]
